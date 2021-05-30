b5ccfd5c827b1396fd42fd9069bf426deeaae4bf
rohideki <71408840+rohideki@users.noreply.github.com>
2021-05-30 13:11:11 -0400

    initial commit

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+require('dotenv/config');
 const express = require('express');
 const routes = require('./routes');
 
@@ -8,4 +9,4 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+app.listen(process.env.PORT);
